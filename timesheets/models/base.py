a583c2594496b61760534c7901dd3784ab9c5d62
Imam Usmani <imam.usmani@haag-streit.com>
2023-01-03 10:41:32 +0100

    timesheet model

--- timesheets/models/base.py
+++ timesheets/models/base.py
@@ -1,14 +1,45 @@
 def main():
     pass
 
+class TimeSheet(object):
 
-class MyClass(object):
+    """Docstring for TimeSheet. """
 
-    """Docstring for MyClass. """
+    def __init__(self, employement_rate):
+        """TODO: to be defined.
 
-    def __init__(self):
-        """TODO: to be defined. """
+        :employement_rate: float
+
+        """
+        self._employement_rate = employement_rate
+        self._df = None
+        self.path = None
+
+    def create_new(self, year, holidays):
+        """create a new dataframe
+
+        :year: int
+        :holidays: list of date
+
+        """
+        df = None
+        self._df = df
+
+    def save(self):
+        """save dataframe in the path define in attributes
+
+        """
         pass
+
+    def load(self):
+        """load df from path
+
+        """
+        pass
+
+    def check_balance(self, date):
+        balance = 0
+        return balance
 
 
 if __name__ == '__main__':
